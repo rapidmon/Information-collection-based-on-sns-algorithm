8bd0ae61d5b3505ee35d5449820ac27a4eb1cd91
rapidmon <ehhwll@hanmail.net>
2026-03-20 14:38:22 +0900

    1회용 cloudflare api 주소 추가

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,3 @@
 // Cloudflare Tunnel URL로 업데이트 필요
 // 로컬 테스트: 'http://localhost:8000/api'
-export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://photographer-frost-economy-republic.trycloudflare.com/api';
